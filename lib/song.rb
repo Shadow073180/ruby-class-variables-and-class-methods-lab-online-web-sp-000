8f5e336c13ccf99574998575df1586e01b5dafc9
David masterson <david73180@gmail.com>
2018-10-03 15:19:10 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+
+   attr_accessor  :name, :artist, :genre
+
+      @@count = 0
+      @@artists = []
+      @@genres = []
+
+
+
+   def initialize(name, artist, genre)
+     @name = name
+     @artist = artist
+     @genre = genre
+     @@artists.push(artist)
+     @@genres.push(genre)
+     @@count += 1
+   end
+
+
+
+
+
+   def self.count
+     @@count
+   end
+
+   def self.artists
+     return @@artists.uniq
+   end
+
+   def self.genres
+     @@genre_count[@genre] ||= []
+     return @@genres.uniq
+   end
+
+   def self.genre_count
+     i = 0
+     genre_count = {}
+    @@genres.each do |x|
+
+
+    end
+
+
+   def self.artist_count
+   end
+
+
+end
